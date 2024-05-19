fbaccd2e9ec27b4f7ff82b715631b9e58f4220fe
GOKUL-KANNAN-P <gokulkannanp.cse2021@citchennai.net>
2024-05-19 08:06:59 +0530

    first

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: "supersecret",
+    secret: "super",
     resave: false,
     saveUninitialized: false,
     cookie: {
